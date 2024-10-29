059e192647a88a3051267fa9c35c655a16f65e45
Pragat Pandya <pragat.pandya@gmail.com>
2024-10-29 23:33:45 +0530

    First Commit: Google Clone.

---
+++ android/app/src/main/kotlin/com/example/google_clone_tutorial/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.google_clone_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_clone_tutorial/colors.dart';
+import 'package:google_clone_tutorial/responsive/mobile_screen_layout.dart';
+import 'package:google_clone_tutorial/responsive/responsive_layout_screen.dart';
+import 'package:google_clone_tutorial/responsive/web_screen_layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Google Clone',
+      home: ResponsiveLayoutScreen(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
+      ),
+    );
+  }
+}
